96bc89fb7300fbd0f1301e13bfe11fcb464bfb17
Andrea Marco Sartori <22cerbero22@gmail.com>
2016-04-17 23:04:39 +1000

    Create client

---
+++ src/Clients/GuzzleAdapter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cerbero\ApiClient\Clients;
+
+use GuzzleHttp\ClientInterface as Guzzle;
+use Guzzle\Http\Client;
+
+/**
+ * Adapter for the Guzzle HTTP client.
+ *
+ * @author    Andrea Marco Sartori
+ */
+class GuzzleAdapter implements ClientInterface
+{
+    /**
+     * The Guzzle client.
+     *
+     * @author  Andrea Marco Sartori
+     * @var     GuzzleHttp\ClientInterface
+     */
+    protected $client;
+
+    /**
+     * Set the dependencies.
+     *
+     * @author    Andrea Marco Sartori
+     * @param    GuzzleHttp\ClientInterface    $client
+     * @return    void
+     */
+    public function __construct(Guzzle $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Process the HTTP request.
+     *
+     * @author    Andrea Marco Sartori
+     * @param    string    $verb
+     * @param    string    $endpoint
+     * @param    array    $options
+     * @return    mixed
+     */
+    public function call($verb, $endpoint, array $options = [])
+    {
+        return $this->client->request($verb, $endpoint, $options);
+    }
+}
