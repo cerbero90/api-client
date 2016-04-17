8db201da31ac9967a1f6b2af286062497744e6b8
Andrea Marco Sartori <22cerbero22@gmail.com>
2016-04-17 23:05:28 +1000

    Create dummy class to test abstract API

---
+++ tests/ConcreteApi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cerbero\ApiClient;
+
+/**
+ * Dummy class for testing AbstractApi.
+ *
+ * @author    Andrea Marco Sartori
+ */
+class ConcreteApi extends AbstractApi
+{
+    //
+}
