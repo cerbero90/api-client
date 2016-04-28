89a5d9a2941fc65f358b0cebe49b9c78fdd068e4
Andrea Marco Sartori <andrea.sartori@fantasycentre.com>
2016-04-28 14:26:59 +1000

    Set up Behat

---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,96 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Cerbero\ApiClient\GitHub;
+use GuzzleHttp\Promise\PromiseInterface;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * The API implementation.
+     *
+     * @var Cerbero\ApiClient\AbstractApi
+     */
+    protected $api;
+
+    /**
+     * The HTTP client response.
+     *
+     * @var mixed
+     */
+    protected $response;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given I have an implementation of GitHub APIs :version
+     */
+    public function iHaveAnImplementationOfGithubApis($version)
+    {
+        $this->api = new GitHub($version);
+    }
+
+    /**
+     * @When I call the endpoint to show the user :user followers
+     */
+    public function iCallTheEndpointToShowTheUserFollowers($user)
+    {
+        $this->response = $this->api->users($user)->followers()->call();
+    }
+
+    /**
+     * @Then I should see a correct response
+     */
+    public function iShouldSeeACorrectResponse()
+    {
+        if ($this->response->getHeader('Status') != ['200 OK']) {
+            throw new Exception('Failed asserting that the status is 200.');
+        }
+
+        if ($this->response->getHeader('Server') != ['GitHub.com']) {
+            throw new Exception('Failed asserting that the called server is GitHub.');
+        }
+    }
+
+    /**
+     * @When I call the endpoint to show the user :user gists
+     */
+    public function iCallTheEndpointToShowTheUserGists($user)
+    {
+        $this->response = $this->api->users($user)->gists()->call();
+    }
+
+    /**
+     * @When I asynchronously call the endpoint to show the user :user gists
+     */
+    public function iAsynchronouslyCallTheEndpointToShowTheUserGists($user)
+    {
+        $this->response = $this->api->users($user)->gists()->then(function () {});
+    }
+
+    /**
+     * @Then I should see a promise as a response
+     */
+    public function iShouldSeeAPromiseAsAResponse()
+    {
+        if (! $this->response instanceof PromiseInterface) {
+            throw new Exception('Failed asserting that the response is a promise.');
+        }
+    }
+}
